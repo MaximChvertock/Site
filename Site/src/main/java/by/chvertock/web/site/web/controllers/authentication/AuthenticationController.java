de406b745eed770b41ef8d7a63a7d1e63fca0f18
Maksim <mchvertock@gmail.com>
2016-11-09 02:02:08 +0300

    update login page

--- Site/src/main/java/by/chvertock/web/site/web/controllers/authentication/AuthenticationController.java
+++ Site/src/main/java/by/chvertock/web/site/web/controllers/authentication/AuthenticationController.java
@@ -1,93 +1,54 @@
 package by.chvertock.web.site.web.controllers.authentication;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import by.chvertock.web.site.model.LoginForm;
-import by.chvertock.web.site.model.entities.Role;
-import by.chvertock.web.site.model.entities.User;
-import by.chvertock.web.site.service.user.UserService;
+import by.chvertock.web.site.service.SecurityService;
 import by.chvertock.web.site.web.utils.ControllerUtils;
 
 @Controller
 public class AuthenticationController {
     
-    @Autowired
-    private UserService userService;
     
     @Autowired 
-    private AuthenticationManager authenticationManager;
-
-//    @RequestMapping("/login")
-//    public String showAuthenticationPage() {
-//        return "authentication/LoginFormPage";
-//    }
+    private SecurityService securityService;
     
-//    @RequestMapping("action/checkLoginForm")
-//    public String checkLoginForm( @ModelAttribute("loginForm") LoginForm loginForm) {
-//	
-//	User user = userService.findByName(loginForm.getUsername());
-//	
-//	System.out.println(user);
-//	
-//	Set<SimpleGrantedAuthority> authorities = new HashSet<SimpleGrantedAuthority>();
-//        for (Role role : user.getRoles()) {
-//            authorities.add(new SimpleGrantedAuthority(role.getType().name()));
-//        }
-//	
-//	org.springframework.security.core.userdetails.User principal =
-//		new org.springframework.security.core.userdetails.User(
-//			user.getName(),
-//			user.getPassword(),
-//			authorities
-//	);
-//	
-//	UsernamePasswordAuthenticationToken auth =
-//		new UsernamePasswordAuthenticationToken(
-//			principal,
-//			loginForm.getPassword(), // use raw password
-//			authorities
-//			);
-//	authenticationManager.authenticate(auth);
-//     
-//	if(auth.isAuthenticated()) {
-//	    SecurityContextHolder.getContext().setAuthentication(auth);
-//	}
-//	
-//        return ControllerUtils.redirect("/login");
-//    }
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "authentication/LoginFormPage";
+    }
     
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logout(
             HttpServletRequest request,
             HttpServletResponse response
     ) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
+        securityService.logout(request, response);
 
         return ControllerUtils.redirect("/login?logout");
     }
     
     @ModelAttribute("loginForm")
     public LoginForm getLoginForm(){
-	return new LoginForm();
+	LoginForm loginForm = new LoginForm();
+	
+//	loginForm.setUsername("Default Username");
+//	loginForm.setPassword("Default Password");
+	
+	return loginForm;
     }
 }
--- Site/src/main/webapp/WEB-INF/views/authentication/LoginFormPage.jsp
+++ Site/src/main/webapp/WEB-INF/views/authentication/LoginFormPage.jsp
@@ -3,6 +3,8 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
+<c:set var="contextPath" value="${pageContext.request.contextPath}"/>
+
 <!DOCTYPE html>
 <html>
 <title><spring:message code="home_page.title" /></title>
@@ -14,9 +16,10 @@
 	<jsp:include page="../common/Navigation.jsp"></jsp:include>
 
 	<jsp:include page="../common/Modal.jsp"></jsp:include>
+		
 
 	<div class="w3-container w3-padding-64">
-		
+	
 		<div class="w3-row">
 			<div class="w3-container w3-quarter"></div>
 		
@@ -28,20 +31,40 @@
 				</div>
 				
 				<!-- Input fields -->
-				<form:form
-					class="w3-container w3-card-4 w3-padding-16 w3-white"
+				<form:form class="w3-container w3-card-4 w3-padding-16 w3-white"
 					method="post"
-					action="action/checkLoginForm"
+					action="${contextPath}/login"
 					modelAttribute="loginForm">
+					
+					<c:if test="${error != null}">
+						<div class="w3-container w3-panel w3-red w3-card-4">
+							<p>${error}</p>
+						</div>
+					</c:if>
+					
+					<c:if test="${message != null}">
+						<div class="w3-container w3-panel w3-green w3-card-4">
+							<p>${message}</p>
+						</div>
+					</c:if>
 					
 					<div class="w3-group">
 						<label class="w3-label"><spring:message code="form.label.name" /></label>
-						<input class="w3-input" type="text" name="username" />
+						<input class="w3-input"
+							type="text"
+							name="username"
+							placeholder="Username"
+							value="${loginForm.username}" 
+							autofocus="autofocus"/>
 					</div>
 					
 					<div class="w3-group">
 						<label class="w3-label"><spring:message code="form.label.password" /></label>
-						<input class="w3-input" type="password" name="password" />
+						<input class="w3-input"
+							type="password" 
+							name="password"
+							placeholder="Password"
+							value="${loginForm.password}" />
 					</div>
 					
 					<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
